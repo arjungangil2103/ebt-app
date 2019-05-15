e699a51060695d9f832fee9ac6f74e0f30358b8b
arjungangil2103 <47173762+arjungangil2103@users.noreply.github.com>
2019-05-15 06:04:33 +0530

    MY FIRST CODE COMMIT IN ELASTICBEANSTALK

--- application.py
+++ application.py
@@ -110,7 +110,7 @@ welcome = """
 </head>
 <body id="sample">
   <div class="textColumn">
-    <h1>Congratulations</h1>
+    <h1>VIRAJE TECH - QUCIK SUCCESS</h1>
     <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
   </div>
   
